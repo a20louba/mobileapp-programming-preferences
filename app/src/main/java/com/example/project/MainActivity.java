1336d8e36da5ff452f7f6558c5c345d4977e9539
a20louba <a20louba@student.his.se>
2022-05-03 14:33:59 +0200

    uppgift 6.
    skapat textView och lagt till findViewById. Detta för att namnet ska lagras.  lagt till edittext för att den ska kunna lagra de namn som skrivs, men inte slutfört det än, behöver mer findViewById för att skapa javaobject.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewname;
+    SharedPreferences preferences;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textViewname = findViewById(R.id.name);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+                // denna kod skulle fungera i en setOnClickListener
+        //SharedPreferences.Editor  editor= preferences.edit();
+        //editor.putString("name","Louise");
+        //editor.apply();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = preferences.getString("name", "inget namn hittat");
+        textViewname.setText(name);
+
+
+
+
+
+    }
 }
