7aa467d5dbff4b60cbc2c769f86d842cdb5997b7
a20louba <a20louba@student.his.se>
2022-05-05 15:51:40 +0200

    Uppgift 6. lagt till second activity, samt xml. kopplat ihop buttons med main med findViewById, SetOnclickListener, Onclick, intent och putExtras. Det som är kvar är att namnet skall enbart synas i secondActivity till en början, för att sedan synas i first när vi closat second.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,15 +17,32 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences preferences;
     EditText editText;
 
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button button = findViewById(R.id.start_second_activity);
+        button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("name", "Louise");
+                    startActivity(intent);
+                }
+        });
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
         textViewname = findViewById(R.id.name);
         preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+
 
                 // denna kod skulle fungera i en setOnClickListener
         //SharedPreferences.Editor  editor= preferences.edit();
@@ -35,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         String name = preferences.getString("name", "inget namn hittat");
         textViewname.setText(name);
+
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.text_view);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
 
 
 
